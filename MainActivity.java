2c2b546df55ca0314c0c7adead9fb774cc12f9f6
Tuoof <72484719+Tuoof@users.noreply.github.com>
2020-10-21 06:59:42 +0700

    Add files via upload
    demo menggambar bentuk di android studio

---
+++ MainActivity.java
@@ -0,0 +1,94 @@
+package com.tuoof.graphicdemo;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+
+    ImageView ourView;
+
+    // This is the entry point to our game
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Do all our drawing in a separate method
+        draw();
+
+        // Make ourView ImageView object the view for the Activity
+        setContentView(ourView);
+    }
+
+    // This is our draw() method
+    public void draw(){
+
+        // Declare an object of type Bitmap
+        Bitmap blankBitmap;
+        // Make it 600 x 600 pixels in size and an appropriate format
+        blankBitmap = Bitmap.createBitmap(600,600,Bitmap.Config.ARGB_8888);
+        // Declare an object of type canvas
+        Canvas canvas;
+        // Initialize it by making its surface our previously created blank bitmap
+        canvas = new Canvas(blankBitmap);
+
+        // Initialize our previously declared member object of type ImageView
+        ourView = new ImageView(this);
+        // Put our blank bitmap on ourView
+        ourView.setImageBitmap(blankBitmap);
+
+        // We now have a surface ready to draw on
+        // But we need something to draw with
+
+        // Declare an object of type Paint
+        Paint paint;
+        // Initialize it ready for painting our canvas
+        paint = new Paint();
+
+        // Make the canvas white
+        canvas.drawColor(Color.argb(255, 255, 255, 255));
+
+        /*
+        // Declare an object of type Bitmap
+        Bitmap bitmapBob;
+        // Initialize it using the bob.png file
+        bitmapBob = BitmapFactory.decodeResource(this.getResources(), R.drawable.bob);
+        // Now draw bob to our canvas
+        canvas.drawBitmap(bitmapBob, 500, 50, paint);
+
+        // Draw a line
+        canvas.drawLine(50,50,250,250,paint);
+
+        // Draw some text
+        canvas.drawText("Game Code School", 50, 50, paint);
+
+        // Draw a pixel
+        canvas.drawPoint(40,50,paint);
+
+        // Draw a circle
+        canvas.drawCircle(350,250,100,paint);
+
+        // Change the brush color
+        paint.setColor(Color.argb(255,  249, 129, 0));
+
+        // Draw a rectangle
+        canvas.drawRect(50,450,500,550,paint);
+
+        // Back to onCreate method to set our canvas as the view
+        */
+        paint.setColor(Color.argb(255,  117, 121, 231));
+        canvas.drawOval(75,425,525,525, paint);
+
+        paint.setColor(Color.argb(255,  226, 126, 96));
+        canvas.drawRoundRect(75,450,525,550, 30, 30, paint);
+
+        paint.setColor(Color.argb(255,  21, 4, 133));
+        float[] pts = {50, 50,250, 250, 250, 250, 500, 50, 500, 50, 50, 50};
+        canvas.drawLines(pts, paint);
+    }
+}
